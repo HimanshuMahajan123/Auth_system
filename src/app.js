3967bec33374415f75a277192195bc4bf2ca31f2
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-12 13:02:22 +0530

    basic project setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+export default app;
