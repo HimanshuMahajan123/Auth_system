1f8011c34534ea95066a72d73dbfc97b3929ec20
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-13 18:25:10 +0530

    implement logout functionality and update routes to include logout endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controllers.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegisterationValidator,
   userLoginValidator,
 } from "../validators/index.js";
 import upload from "../middlewares/multer.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -18,5 +23,8 @@ router.post(
   registerUser
 );
 router.post("/login", userLoginValidator(), validate, loginUser);
+
+//secured routes
+router.post("/logout", verifyJWT, logoutUser);
 
 export default router;
